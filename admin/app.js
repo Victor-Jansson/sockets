3f37c7f1ffb5867af1fbc9398139ee465a5741fc
Victor-Jansson <victor.jansson@gmail.com>
2016-11-30 17:16:33 +0100

    Admin page can list all active connections

---
+++ admin/app.js
@@ -0,0 +1,23 @@
+define('app', [
+		'node_modules/knockout/build/output/knockout-latest',
+		'node_modules/socket.io-client/dist/socket.io.js',
+		'node_modules/lodash/lodash.js'
+	], function(ko, io, _) {
+		var socket = io.connect('http://localhost:3000');
+		
+		socket.on('users changed', function(clients) {
+			model.users([]);
+
+			_.forEach(clients, function(client) {
+				model.users.push(client);
+			});
+
+		})
+
+		var model = {
+			users: ko.observableArray([])
+		}
+
+	ko.applyBindings(model);
+});
+
